f2498b961314a03b03c27ff8da927c6e3bb4854b
XinZhang <zhangxin19@meituan.com>
2016-03-31 08:39:26 +0800

    Update font scripts.

--- tools/buildFont.sh
+++ tools/buildFont.sh
@@ -1,7 +1,8 @@
-files='views/**/*.ejs src/*.js'
-
+files=`find views src -type f \( -name '*.html' -or -name '*.*js' -or -name '*.*css' \) | tr '\n' ' '`
+echo $files
 # characters=`grep -E -o -h [^0-9a-zA-Z[:space:][:punct:]]+ $files | sort -u | paste -sd '\0' -`
-characters=`grep -E -o -h -r . $files | sort -u | paste -sd '\0' -`
+# characters=`grep -E -o -h -r . $files | sort -u | paste -sd '\0' -`
+characters=`grep -E -o -h -r . $files | sort -u | tr '\n' '\0'`
 echo $characters
 
 fontmin src/*.ttf public/assets/fonts -t $characters
