d64c124d4f92a4b27517551ba1c2a706ba48ff61
XinZhang <zhangxin19@meituan.com>
2016-03-31 16:00:07 +0800

    Update build scripts.

--- tools/buildFont.sh
+++ tools/buildFont.sh
@@ -1,4 +1,4 @@
-files=`find views src -type f \( -name '*.html' -or -name '*.*js' -or -name '*.*css' \) | tr '\n' ' '`
+files=`find src -type f \( -name '*.html' -or -name '*.*js' -or -name '*.*css' \) | tr '\n' ' '`
 echo $files
 # characters=`grep -E -o -h [^0-9a-zA-Z[:space:][:punct:]]+ $files | sort -u | paste -sd '\0' -`
 # characters=`grep -E -o -h -r . $files | sort -u | paste -sd '\0' -`
