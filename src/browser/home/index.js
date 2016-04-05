4697c5706f6d785bd13352920a19553c359ab30c
XinZhang <zhangxin19@meituan.com>
2016-04-06 00:28:18 +0800

    Add ant design.

--- src/browser/home/index.js
+++ src/browser/home/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DatePicker } from 'antd';
 import s from './index.scss';
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello</h1>;
-  }
-}
-
-ReactDOM.render(<Hello />, document.getElementById('test'));
+ReactDOM.render(<DatePicker />, document.getElementById('test'));
